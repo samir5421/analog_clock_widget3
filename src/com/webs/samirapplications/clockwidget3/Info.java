6c5c095deb6bf5bac220046b099df9e087bdf64d
Samir <samir314159@gmail.com>
2010-03-12 21:06:13 -0600

    first commit

---
+++ src/com/webs/samirapplications/clockwidget3/Info.java
@@ -0,0 +1,13 @@
+package com.webs.samirapplications.clockwidget3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Info extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState); setContentView(R.layout.info);
+    }
+}
